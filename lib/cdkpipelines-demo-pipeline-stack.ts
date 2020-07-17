cf40897fd7077bfd65c4d465126f44026bb78aa3
kdirschl <info@audioshaft.com>
2020-07-17 15:01:10 -0400

    Add PreProd stage

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,18 +1,19 @@
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
-import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+import * as codepipeline from '@aws-cdk/aws-codepipeline'
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions'
+import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core'
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines'
+import { CdkpipelinesDemoStage } from './cdkpipeline-demo-stage'
 
 /**
  * The stack that defines the application pipeline
  */
 export class CdkpipelinesDemoPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const sourceArtifact = new codepipeline.Artifact();
-    const cloudAssemblyArtifact = new codepipeline.Artifact();
- 
+    const sourceArtifact = new codepipeline.Artifact()
+    const cloudAssemblyArtifact = new codepipeline.Artifact()
+
     // @ts-ignore
     const pipeline = new CdkPipeline(this, 'Pipeline', {
       // The pipeline name
@@ -30,17 +31,20 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
         trigger: codepipeline_actions.GitHubTrigger.POLL,
       }),
 
-       // How it will be built and synthesized
-       synthAction: SimpleSynthAction.standardNpmSynth({
-         sourceArtifact,
-         cloudAssemblyArtifact,
-         
-         // We need a build step to compile the TypeScript Lambda
-         buildCommand: 'npm run build'
-       }),
-    });
+      // How it will be built and synthesized
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+
+        // We need a build step to compile the TypeScript Lambda
+        buildCommand: 'npm run build',
+      }),
+    })
 
     // This is where we add the application stages
-    // ...
+    // @ts-ignore
+    pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+      env: { account: '271657195655', region: 'us-west-2' }
+    }))
   }
 }
