1f76e1829bdb0694d2d41a73ec0a713b5856df9b
kdirschl <info@audioshaft.com>
2020-07-17 14:12:47 -0400

    reverting

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -24,7 +24,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
         actionName: 'GitHub',
         output: sourceArtifact,
         // @ts-ignore
-        oauthToken: 'b5eeb0646c62764c180511ee74a2be006a3dc96a',
+        oauthToken: SecretValue.secretsManager('github-token'),
         owner: 'badfun',
         repo: 'cdkpipelines-demo',
         trigger: codepipeline_actions.GitHubTrigger.POLL,
