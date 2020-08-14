3afbc606285ad7d0ae1249420eaa179403692583
kdirschl <info@audioshaft.com>
2020-08-14 11:43:48 -0400

    updated package.json

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
-import { App } from '@aws-cdk/core';
-import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { App } from '@aws-cdk/core'
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack'
+import { CdkpipelinesDemoStage } from '../lib/cdkpipeline-demo-stage'
 
-const app = new App();
+const app = new App()
 
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
-  env: { account: '271657195655', region: 'us-west-2' },
-});
+  env: {
+    account: '271657195655',
+    region: 'us-west-2',
+  },
+})
 
-app.synth();
+new CdkpipelinesDemoStage(app, 'Dev', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+})
+
+app.synth()
