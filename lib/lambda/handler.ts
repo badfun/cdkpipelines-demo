254723b0f6119d4943d12335bd38b54ac122846a
kdirschl <info@audioshaft.com>
2020-08-14 12:38:29 -0400

    adding stripe test

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,4 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
+import { Stripe } from 'stripe'
+
+const stripe = new Stripe('sk_0HA7TRg4Y86YGH7dcV0gBQ4Gge6d3' || '', { apiVersion: '2020-03-02'})
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
