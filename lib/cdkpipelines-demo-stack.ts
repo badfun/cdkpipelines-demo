beacce3aa5f4ead6b1c56267c7bd4412a0cba64f
kdirschl <info@audioshaft.com>
2020-08-14 14:14:04 -0400

    another attempt at packaging node modules

--- lib/cdkpipelines-demo-stack.ts
+++ lib/cdkpipelines-demo-stack.ts
@@ -18,12 +18,11 @@ export class CdkpipelinesDemoStack extends Stack {
     // The Lambda function that contains the functionality
     const handler = new lambda.Function(this, 'Lambda', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      handler: 'lib/lambda/handler.handler',
-      code: lambda.Code.fromAsset(path.resolve(__dirname, '../')),
+      handler: 'handler.handler',
+      code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda')),
     });
 
-
-
+    
     // An API Gateway to make the Lambda web-accessible
     const gw = new apigw.LambdaRestApi(this, 'Gateway', {
       description: 'Endpoint for a simple Lambda-powered web service',
