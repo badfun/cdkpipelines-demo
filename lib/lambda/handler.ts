5ce0a607cdeec61453f49d1fb222357bc112eccf
kdirschl <info@audioshaft.com>
2020-08-14 18:45:46 -0400

    small change to force upload check

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -5,7 +5,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello from lambda function',
+    body: 'Hello from the lambda function',
     statusCode: 200
   }
 }
