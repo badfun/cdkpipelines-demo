fd9cb2a19fe37150da8e86a2db163bbc83829a53
kdirschl <info@audioshaft.com>
2020-08-15 14:04:20 -0400

    adding import statement and a console.log

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,9 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
-// import { Stripe } from 'stripe'
 
-// const stripe = new Stripe('sk_0HA7TRg4Y86YGH7dcV0gBQ4Gge6d3' || '', { apiVersion: '2020-03-02'})
+import { v4 as uuidv4 } from 'uuid'
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+
+  console.log('UUID test: ', uuidv4())
+
   return {
     body: 'Hello from the lambda function',
     statusCode: 200
