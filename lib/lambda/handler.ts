cb2c120a7b6097f3e3e3b1aea3ce45757cec3320
kdirschl <info@audioshaft.com>
2020-08-15 23:16:51 -0400

    uncommenting dependencies now

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,10 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
 
-// import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
-  // console.log('UUID test: ', uuidv4())
+  console.log('UUID test: ', uuidv4())
 
   return {
     body: 'Hello from the lambda function',
