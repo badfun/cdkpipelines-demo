6fa8d07c8d63fb95de7bbb41262446e2e9f43f11
kdirschl <info@audioshaft.com>
2020-08-15 23:43:00 -0400

    force commit

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
-  const stripe = new Stripe('sk_0HA7TRg4Y86YGH7dcV0gBQ4Gge6d4' || '', { apiVersion: '2020-03-02'})
+  const stripe = new Stripe('sk_0HA7TRg4Y86YGH7dcV0gBQ4Gge6d3' || '', { apiVersion: '2020-03-02'})
 
   return {
     body: 'Hello from the lambda function',
