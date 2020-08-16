e1d3077c4daa5acd132607626d0da98dc19d1fb8
kdirschl <info@audioshaft.com>
2020-08-15 20:00:25 -0400

    attempting to install dependencies with buildCommand

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -38,7 +38,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
         // installCommand: 'cd lib/lambda && npm ci && cd ../../ && npm ci',
         
         // Build step to compile the TypeScript Lambda
-        buildCommand: 'npm run build',
+        buildCommand: 'cd lib/lambda && npm ci && cd ../../ && npm ci',
       }),
     })
 
